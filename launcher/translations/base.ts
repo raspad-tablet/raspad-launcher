d946b58c1bb7f68f20c4fb2c3b7ae7248b402eaa
purewasa <purewasa@web.de>
2021-10-23 21:49:58 +0200

    Update .ts translation files
    Source file paths and line numbers are updated for the translated strings.
    6 new unfinished translation have been added.

--- launcher/translations/base.ts
+++ launcher/translations/base.ts
@@ -4,82 +4,112 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../src/main.qml" line="63"/>
+        <location filename="../qml/main.qml" line="62"/>
         <source>Network Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="121"/>
-        <location filename="../src/main.qml" line="398"/>
+        <location filename="../qml/main.qml" line="95"/>
         <source>Home</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="126"/>
+        <location filename="../qml/main.qml" line="100"/>
         <source>Programming</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="131"/>
+        <location filename="../qml/main.qml" line="105"/>
         <source>Education</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="136"/>
+        <location filename="../qml/main.qml" line="110"/>
         <source>Office</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="141"/>
+        <location filename="../qml/main.qml" line="115"/>
         <source>Internet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="146"/>
+        <location filename="../qml/main.qml" line="120"/>
         <source>Sound &amp; Video</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="151"/>
+        <location filename="../qml/main.qml" line="125"/>
         <source>Graphics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="156"/>
+        <location filename="../qml/main.qml" line="130"/>
         <source>Games</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="161"/>
+        <location filename="../qml/main.qml" line="135"/>
         <source>Accessories</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="166"/>
+        <location filename="../qml/main.qml" line="140"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="171"/>
+        <location filename="../qml/main.qml" line="145"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="176"/>
+        <location filename="../qml/main.qml" line="150"/>
         <source>Run</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="285"/>
+        <location filename="../qml/main.qml" line="268"/>
         <source>Recommend:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="361"/>
+        <source>Desktop file &apos;%1&apos;
+contains no valid Exec line!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="368"/>
+        <source>Invalid desktop file: &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="392"/>
+        <source>Error starting command of desktop file: &apos;%1&apos;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>run</name>
     <message>
-        <location filename="../src/run.qml" line="23"/>
+        <location filename="../qml/run.qml" line="25"/>
         <source>Enter the command you want to execute: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="75"/>
+        <source>Text is empty (or only consists of whitespace).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="82"/>
+        <source>Invalid command line!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="97"/>
+        <source>Error starting command!</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- launcher/translations/zh_CN.ts
+++ launcher/translations/zh_CN.ts
@@ -4,83 +4,113 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../src/main.qml" line="63"/>
+        <location filename="../qml/main.qml" line="62"/>
         <source>Network Error</source>
         <translation>网络错误</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="121"/>
-        <location filename="../src/main.qml" line="398"/>
+        <location filename="../qml/main.qml" line="95"/>
         <source>Home</source>
         <translation>主页</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="126"/>
+        <location filename="../qml/main.qml" line="100"/>
         <source>Programming</source>
         <translation>编程</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="131"/>
+        <location filename="../qml/main.qml" line="105"/>
         <source>Education</source>
         <translation>教育</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="136"/>
+        <location filename="../qml/main.qml" line="110"/>
         <source>Office</source>
         <translation>办公软件</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="141"/>
+        <location filename="../qml/main.qml" line="115"/>
         <source>Internet</source>
         <translation>网络软件</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="146"/>
+        <location filename="../qml/main.qml" line="120"/>
         <source>Sound &amp; Video</source>
         <translation>音视频</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="151"/>
+        <location filename="../qml/main.qml" line="125"/>
         <source>Graphics</source>
         <translation>图像</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="156"/>
+        <location filename="../qml/main.qml" line="130"/>
         <source>Games</source>
         <translation>游戏</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="161"/>
+        <location filename="../qml/main.qml" line="135"/>
         <source>Accessories</source>
         <translation>工具箱</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="166"/>
+        <location filename="../qml/main.qml" line="140"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="171"/>
+        <location filename="../qml/main.qml" line="145"/>
         <source>Preferences</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="176"/>
+        <location filename="../qml/main.qml" line="150"/>
         <source>Run</source>
         <translation>运行</translation>
     </message>
     <message>
-        <location filename="../src/main.qml" line="285"/>
+        <location filename="../qml/main.qml" line="268"/>
         <source>Recommend:</source>
         <translation>推荐应用:</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="361"/>
+        <source>Desktop file &apos;%1&apos;
+contains no valid Exec line!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="368"/>
+        <source>Invalid desktop file: &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="392"/>
+        <source>Error starting command of desktop file: &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>run</name>
     <message>
-        <location filename="../src/run.qml" line="23"/>
+        <location filename="../qml/run.qml" line="25"/>
         <source>Enter the command you want to execute: </source>
         <translation>输入你想要执行的命令： </translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="75"/>
+        <source>Text is empty (or only consists of whitespace).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="82"/>
+        <source>Invalid command line!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/run.qml" line="97"/>
+        <source>Error starting command!</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
